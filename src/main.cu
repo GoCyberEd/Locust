9d392d9329269aa957dc6ef8b190cda18a935810
Edward Atter <ed@gocybered.com>
2018-11-25 02:12:52 -0500

    Finish word count

--- src/main.cu
+++ src/main.cu
@@ -73,6 +73,33 @@ __global__ void kernMap(KeyValuePair** in, KeyValuePair** out, int length) {
 	map(*in[i], out, i * EMITS_PER_LINE);
 }
 
+__host__ void reduce(int start, int end, KeyValuePair** in, KeyValuePair** out, int n) {
+	char* key = in[start]->key;
+	char value[50];
+	sprintf(value, "%i", end-start);
+	out[n] = new KeyValuePair(key, value);
+}
+
+__host__ void cpuReduce(KeyValuePair** in, KeyValuePair** out, int length) {
+	if (in[0] == NULL) return;
+
+	char* key = in[0]->key;
+	int start = 0;
+	int n = 0;
+	for (int i = 0; i < length; i++) {
+		if (in[i] == NULL || strcmp(key, in[i]->key) != 0) {
+			reduce(start, i, in, out, n);
+			if(in[i] == NULL) {
+				return; //Sorted, so we must be at the end
+			}
+
+			key = in[i]->key;
+			start = i;
+			n++; //TODO this math doesn't work out, ensure we can't overflow keys
+		}
+	}
+}
+
 __host__ int main(int argc, char* argv[]) {
 	std::cout << "Running\n";
 	// Load file
@@ -84,7 +111,6 @@ __host__ int main(int argc, char* argv[]) {
 
 	// Map stage
 	KeyValuePair* map_kvs[MAX_EMITS] = {NULL};
-	for (int i = 0; i < MAX_EMITS; i++) { map_kvs[i] = 0; }
 	cpuMap(file_kvs, map_kvs, length);
 	//printKeyValues(map_kvs, MAX_EMITS);
 
@@ -101,10 +127,13 @@ __host__ int main(int argc, char* argv[]) {
 	thrust::sort(dev_ptr, dev_ptr + MAX_EMITS, KVComparator());
 	*/
 	std::sort(map_kvs, map_kvs + MAX_EMITS, KVComparator());
-	printKeyValues(map_kvs, MAX_EMITS);
+	//printKeyValues(map_kvs, MAX_EMITS);
 
 	// Reduce stage
-	//TODO
+	KeyValuePair* reduce_kvs[MAX_EMITS] = {NULL};
+	cpuReduce(map_kvs, reduce_kvs, MAX_EMITS);
+	std::sort(reduce_kvs, reduce_kvs + MAX_EMITS, KVComparator());
+	printKeyValues(reduce_kvs, MAX_EMITS);
 
 	std::cout << "Done\n";
 	return 0;
